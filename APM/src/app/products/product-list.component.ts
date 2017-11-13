40c928d681f299c5fd3cea7538f73f0c9dc81c9c
HangTenKen <HangTenKen@gmail.com>
2017-11-12 22:06:20 -0800

    Module 6:Data Binding and Pipes Module 7: More on Components

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,12 +1,28 @@
-import { Component }from '@angular/core';
+import { Component, OnInit }from '@angular/core';
+import { IProduct } from './product';
 
 @Component ({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
-    products: any[] = [
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+
+    _listFilter: string;
+    get listFilter(): string {
+        return this._listFilter
+    }
+    set listFilter(value:string) {
+        this._listFilter = value;
+        this.filteredProducts=this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+
+    filteredProducts: IProduct[];
+    products: IProduct[] = [
         {
             "productId" : 2,
             "productName" : "Satelite Broadband 5Tb/s",
@@ -15,7 +31,7 @@ export class ProductListComponent {
             "description" : "Lightening 5Tb/s Satelite Broadband",
             "price" : 14.99,
             "starRating" : 5.0,
-            "imageUrl" : "http://openclipart.org/pdf/144799/power-thumb.pdf"
+            "imageUrl" : "https://openclipart.org/download/144799/power-thumb.svg"
         },
         {
             "productId" : 5,
@@ -25,7 +41,7 @@ export class ProductListComponent {
             "description" : "SpaceX TShirt",
             "price" : 19.99,
             "starRating" : 4.5,
-            "imageUrl" : "http://openclipart.org/pdf/274172/elonmuskmoon.pdf"
+            "imageUrl" : "https://openclipart.org/image/2400px/svg_to_png/274172/elonmuskmoon.png"
         },
         {
             "productId" : 7,
@@ -33,9 +49,27 @@ export class ProductListComponent {
             "productCode" : "LF-SPX2018",
             "releaseDate" : "March 23, 2018",
             "description" : "Lightening 5Tb/s Satelite Broadband",
-            "price" : 300100.99,
+            "price" : 3200100.99,
             "starRating" : 5.0,
-            "imageUrl" : "http://openclipart.org/pdf/214100/scifirocketplanet.pdf"
+            "imageUrl" : "https://openclipart.org/download/214100/scifirocketplanet.svg"
         }
     ];
+
+    constructor() {
+        this.filteredProducts = this.products;
+        this.listFilter = 'Broadband';
+    }
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+        console.log('In OnInit');
+    }
 }
