ccb63f753c1a4739a1e156f640e773e086d98a41
HangTenKen <HangTenKen@gmail.com>
2017-11-26 19:35:45 -0800

    10. Retrieving Data Using HTTP
         Observables and Reactive Extensions, sending Http Request, Exception Handling, Subscribing to an Observable

--- APM/src/app/products/product.service.ts
+++ APM/src/app/products/product.service.ts
@@ -1,41 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
+
 import { IProduct } from './product';
 
 @Injectable()
 export class ProductService {
+    private _productUrl = './api/products/products.json'; // This should be an actual service url 
 
-    getProducts(): IProduct[] {
-        return [
-            {
-                "productId" : 2,
-                "productName" : "Satelite Broadband 5Tb/s",
-                "productCode" : "BB-5T",
-                "releaseDate" : "March 23, 2018",
-                "description" : "Lightening 5Tb/s Satelite Broadband",
-                "price" : 14.99,
-                "starRating" : 5.0,
-                "imageUrl" : "https://openclipart.org/download/144799/power-thumb.svg"
-            },
-            {
-                "productId" : 5,
-                "productName" : "SpaceX TShirt",
-                "productCode" : "TS-SPX33",
-                "releaseDate" : "March 23, 2018",
-                "description" : "SpaceX TShirt",
-                "price" : 19.99,
-                "starRating" : 4.5,
-                "imageUrl" : "https://openclipart.org/image/2400px/svg_to_png/274172/elonmuskmoon.png"
-            },
-            {
-                "productId" : 7,
-                "productName" : "Lunar Flyby",
-                "productCode" : "LF-SPX2018",
-                "releaseDate" : "March 23, 2018",
-                "description" : "Lightening 5Tb/s Satelite Broadband",
-                "price" : 3200100.99,
-                "starRating" : 5.0,
-                "imageUrl" : "https://openclipart.org/download/214100/scifirocketplanet.svg"
-            }            
-        ];
+    constructor(private _http: HttpClient) {}
+
+    getProducts(): Observable<IProduct[]> {
+        return this._http.get<IProduct[]>(this._productUrl)
+            .do(data => console.log('All: ' + JSON.stringify(data)))
+            .catch(this.handleError);
+    }
+
+    private handleError(err: HttpErrorResponse) {
+        console.log(err.message);
+        return Observable.throw(err.message);
     }
 }
